a3263b63e2d81319fa6e9f5779af3caf96cafa86
Stefano Romanelli <53661102+RayCatcherS@users.noreply.github.com>
2026-06-06 17:59:16 +0200

    Update ButtonCallToAction.tsx

--- src/assets/components/ButtonCallToAction.tsx
+++ src/assets/components/ButtonCallToAction.tsx
@@ -9,10 +9,10 @@ const ButtonCallToAction: React.FC<{ callToAction: CallToAction }> = ({ callToAc
     return (
         <button
             onClick={handleClick}
-            className={`px-5 py-2.5 
+            className={`px-5 py-2.5 inline-block
                 text-white text-description font-melodi-light rounded-lg
-                bg-violet-900 shadow-md transition-all duration-300 ease-out
-                hover:bg-violet-700 hover:-translate-y-1 hover:shadow-[0_0_15px_rgba(139,92,246,0.5)]
+                bg-violet-900 shadow-md transform transition-all duration-300 ease-out
+                hover:bg-violet-700 hover:-translate-y-1.5 hover:scale-105 hover:shadow-[0_0_15px_rgba(139,92,246,0.5)]
                 active:translate-y-0 active:scale-95`}>
             <div className='pr-1.5 pl-1.5 pt-1 pb-1'>{callToAction.callToActionText}</div>
         
